8a2cfd89803d14aa6a447aa455a779e4b32f94e4
Brandon Mendoza <mendo229@cougars.csusm.edu>
2019-07-16 11:34:35 -0700

    first commit

--- API_LECTURE/main.py
+++ API_LECTURE/main.py
@@ -1,1 +1,13 @@
 #ADD STUFF HERE BRANDON
+import webapp2
+
+from google.appengine.api import urlfetch
+import json
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.write("Hello World")
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
