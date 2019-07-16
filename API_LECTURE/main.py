12d931f4edb3be4306427b8f626331286bb54a1e
Brandon Mendoza <mendo229@cougars.csusm.edu>
2019-07-16 11:41:19 -0700

    added arrays for multiple answers

--- API_LECTURE/main.py
+++ API_LECTURE/main.py
@@ -12,8 +12,21 @@ class MainPage(webapp2.RequestHandler):
         trivia_as_json = json.loads(trivia_response)
         first_result = trivia_as_json['results'][0]
         trivia_question = first_result['question']
-        self.response.write(trivia_question)
-        
+        correct_answer = first_result['correct_answer']
+        incorrect_answers = first_result['incorrect_answers']
+
+        all_answer_options = [correct_answer]
+        for answer in incorrect_answers:
+            all_answer_options.append(answer)
+
+        random.shuffle(all_answer_options)
+
+        self.response.write("<h2>" + trivia_question + "</h2>")
+        self.response.write(all_answer_options)
+
+
+
+
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
