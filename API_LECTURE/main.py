28db37e60b6d27a27ad9704f75d41e3005f55fae
Brandon Mendoza <mendo229@cougars.csusm.edu>
2019-07-16 11:36:36 -0700

    added to mainpage class

--- API_LECTURE/main.py
+++ API_LECTURE/main.py
@@ -1,12 +1,19 @@
 #ADD STUFF HERE BRANDON
 import webapp2
+import random
 
 from google.appengine.api import urlfetch
 import json
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.write("Hello World")
+        trivia_endpoint_url = "https://opentdb.com/api.php?amount=1&category=10&difficulty=easy&type=multiple"
+        trivia_response= urlfetch.fetch(trivia_endpoint_url).content
+        trivia_as_json = json.loads(trivia_response)
+        first_result = trivia_as_json['results'][0]
+        trivia_question = first_result['question']
+        self.response.write(trivia_question)
+        
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
